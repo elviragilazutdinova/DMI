f2658d20cb252bb6d652bf0e23ca22f13360f2da
Guest <elvira113@mail.ru>
2017-10-11 11:24:47 +0300

    20171011_11_25

--- shell_operators.sh
+++ shell_operators.sh
@@ -1,5 +1,81 @@
 #!/bin/bash
 
+# 5. piemērs - operacijas ar 
+a=$1
+b=$2
+
+if [ $a == $b ]
+then
+echo "a = b"
+fi
+
+if [ $a != $b ]
+then 
+echo "a is not equal to b"
+fi
+
+if [ $a -eq $b ]
+then 
+echo "a -eq b : a is equal to b"
+else 
+echo "a -eq b : a is not equal to b"
+fi
+
+if [ $a -ne $b ]
+then 
+echo "a -ne b : a is not equal to b"
+else 
+echo "a -ne b : a  not equal to b"
+fi
+
+if [ $a -gt $b ]
+then 
+echo "a -gt b : a is greater than b"
+else 
+echo "a -gt b : a is not greater than  b"
+fi
+
+if [ $a -lt $b ]
+then 
+echo "a -lt b : a is less than b"
+else 
+echo "a -lt b : a is not less than b"
+fi
+
+if [ $a -ge $b ]
+then 
+echo "a -ge b : a is greater or equal to b"
+else 
+echo "a -ge b : a is not greater or equal to b"
+fi
+
+if [ $a -le $b ]
+then 
+echo "a -le b : a is less or equal to b"
+else 
+echo "a -le b : a is not less or equal to b"
+fi
+
+
+
+: <<'END'
+# 4. piemērs - operacijas ar argumentiem
+
+a=$1
+b=$2
+val41=`expr $a + $b`
+echo "Total value a+b = "$val41
+val42=`expr $a \*  $b`
+echo "Total value a*b = "$val42
+val43=`expr $a / $b`
+echo "Total value a/b = "$val43
+val44=`expr $a % $b`
+echo "Total value a%b = "$val44
+val45=`expr $a - $b`
+echo "Total value a-b = "$val45
+
+echo $*
+
 #3. piemērs - operacijas ar mainīgājiem
 
 a=29
@@ -14,11 +90,6 @@ val34=`expr $a % $b`
 echo "Total value a%b = "$val34
 val35=`expr $a - $b`
 echo "Total value a-b = "$val35
-
-: <<'END'
-#if= a = $b
-#then
-#echo "Total value : "
 
 # 2. piemērs - orepacijas ar konstantēm
 
