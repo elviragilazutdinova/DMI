a9cfd935ecf6e000fbac5bf70c4fbcb74996986a
lionika88@mail.ru <elvira113@mail.ru>
2018-01-05 18:46:22 +0200

    20180105_18_50

--- PYTHON/LD3.py
+++ PYTHON/LD3.py
@@ -1,26 +1,27 @@
 # -*- coding: utf-8 -*-
-import numpy as np #1
-import matplotlib.pyplot as plt #2
+import numpy as np 
+import matplotlib.pyplot as plt 
 
-def mans_sinuss(x): #6b
+def mans_arcsinuss(x): 
     k = 0
-    a = (-1)**0*x**1/(1)
+    a = 1*x**1/1
     S = a
     while k < 500:
         k = k + 1      
-        R = (-1) * x**2/((2*k)*(2*k+1))
+        R = x**2*((2*k-1)**2)/(2*(2*k+1)*k)
         a = a * R
         S = S + a
     return S
 
-a = 0 #3
-b = 3 * np.pi #4
+a = -.99 
+b = .99 
 # funkcijas zīmēšana
-x = np.arange(a,b,0.1) #5
-y = mans_sinuss(x) #6a
-plt.plot(x,y) #7
-plt.grid() #8
-#plt.show() #9
+x = np.arange(a,b,0.1) 
+y = mans_arcsinuss(x)
+yy = np.arcsin(x)
+plt.plot(x,y,'r',x,yy,'b') 
+plt.grid() 
+#plt.show() 
 
 # funkcijas atvasinājuma aprēķins
 n = len(x)
@@ -29,7 +30,8 @@ for i in range(n-1):
     #print i, y[i], y[i+1]
     #print (y[i+1]-y[i])/(x[i+1]-x[i])
     y_prim.append((y[i+1]-y[i])/(x[i+1]-x[i]))
-plt.plot(x[:n-1],y_prim)
+yy_prim = 1/np.sqrt(1-x*x)
+plt.plot(x[:n-1],y_prim,'g',x,yy_prim)
 plt.show()
 
-
+ 
